5c817fe6bb89759137200f7ec1dd685238b76168
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-20 18:41:50 +0300

    Refactor import stories in tests

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import { composeStories } from '@storybook/testing-react';
-
+// stories
 import * as TaskListStories from './TaskList.stories';
 
 const { WithPinnedTasks } = composeStories(TaskListStories);
