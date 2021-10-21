5bd6f46fd8db47c1a3694d5c3573768fd5a1b4e3
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-21 23:57:05 +0300

    Update TaskList story

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,16 +1,16 @@
 import React from 'react';
 // components
-import TaskList from './TaskList';
+import { PureTaskList } from './TaskList';
 // stories
 import * as TaskStories from './Task.stories';
 
 export default {
-  component: TaskList,
+  component: PureTaskList,
   title: 'TaskList',
   decorators: [(story) => <div style={{ padding: '3rem' }}>{story()}</div>],
 };
 
-const Template = (args) => <TaskList {...args} />;
+const Template = (args) => <PureTaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
